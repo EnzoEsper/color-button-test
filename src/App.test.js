4980706b0b063de02e22368a630af4f289ceea53
enzoesper <enzoesper@hotmail.com>
2021-06-25 16:40:50 -0300

    adding a checkbox and testing the initial conditions

--- src/App.test.js
+++ src/App.test.js
@@ -18,8 +18,13 @@ test('button has correct initial color', () => {
   expect(colorButton.textContent).toBe('Change to red');
 });
 
-// test('button turns blue when clicked', () => {
-//   render(<App />);
-//   const colorButton = screen.getByRole('button', {name: 'Change to blue'});
+test('initial conditions', () => {
+  render(<App />);
+  // check that the button starts out enabled
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'});
+  expect(colorButton).toBeEnabled();
 
-// });
+  // check that the checkbox starts out unchecked
+  const checkBox = screen.getByRole('checkbox');
+  expect(checkBox).not.toBeChecked();
+});
